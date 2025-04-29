fc0284cc8d645baa2ad4f4901734735f0d9e4323
We1l6.f <we1l6.f@gmail.com>
2025-04-29 15:43:12 +0300

    feat: added 3 buttons in main menu

--- src/scene/menuScene.cpp
+++ src/scene/menuScene.cpp
@@ -8,15 +8,32 @@ void MenuScene::HandleInput(float deltaTime)
     {
         m_game->ChangeScene(new GameScene(m_game));
     }
+    if (m_buttons[0].IsClicked())
+    {
+        m_game->ChangeScene(new GameScene(m_game));
+    }
+    else if (m_buttons[2].IsClicked())
+    {
+        CloseWindow();
+    }
 }
 
 
 void MenuScene::Render()
 {
-    DrawText("Start", 200, 250, 20, WHITE);
-    DrawText("Settings", 200, 270, 20, WHITE);
-    DrawText("Exit", 200, 290, 20, WHITE);
+    DrawTexture(LoadTexture("resources/Menu.png"), 0, 0, WHITE);
+
+    for (const auto &button : m_buttons)
+    {
+        button.Draw();
+    }
 }
 
 
-void MenuScene::Update(float deltaTime) {}
+void MenuScene::Update(float deltaTime)
+{
+    for (auto &button : m_buttons)
+    {
+        button.Update();
+    }
+}
--- src/scene/menuScene.h
+++ src/scene/menuScene.h
@@ -5,19 +5,46 @@
 #define MENUSCENE_H
 
 
+#include "../button/button.h"
 #include "../game/game.h"
 #include "gameScene.h"
 #include "raylib.h"
 #include "scene.h"
 
-
 class MenuScene final : public Scene
 {
   public:
-    explicit MenuScene(Game *game) : Scene(game) {}
+    explicit MenuScene(Game *game) : Scene(game)
+    {
+        float buttonWidth = 200;
+        float buttonHeight = 40;
+        float startY = GetScreenHeight() / 2 - buttonHeight;
+
+        m_buttons.emplace_back("Start",
+                               Rectangle{GetScreenWidth() / 2 - buttonWidth / 2,
+                                         startY, buttonWidth, buttonHeight},
+                               Color{17, 27, 29, 255}, Color{57, 75, 76, 255},
+                               WHITE);
+
+        m_buttons.emplace_back(
+            "Settings",
+            Rectangle{GetScreenWidth() / 2 - buttonWidth / 2,
+                      startY + buttonHeight + 10, buttonWidth, buttonHeight},
+            Color{17, 27, 29, 255}, Color{57, 75, 76, 255}, WHITE);
+
+        m_buttons.emplace_back("Exit",
+                               Rectangle{GetScreenWidth() / 2 - buttonWidth / 2,
+                                         startY + 2 * (buttonHeight + 10),
+                                         buttonWidth, buttonHeight},
+                               Color{17, 27, 29, 255}, Color{57, 75, 76, 255},
+                               WHITE);
+    }
     void HandleInput(float deltaTime) override;
     void Update(float deltaTime) override;
     void Render() override;
+
+  private:
+    std::vector<Button> m_buttons;
 };
 
 
